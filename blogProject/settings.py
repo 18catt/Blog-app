6bef379562ef8e28d0d2299c95cc894b368accc2
18catt <98676305+18catt@users.noreply.github.com>
2024-01-18 18:38:32 +0530

    Add files via upload

--- blogProject/settings.py
+++ blogProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-8j_uz++l5qe9^)!-cpkiiss1=q=y8_v!=d(35#_x^$t&20=j6f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
